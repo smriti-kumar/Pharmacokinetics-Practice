2d06247b55e0f5a24db37c6f772edeee2f92a30c
Smriti Kumar <mailsmritikumar@gmail.com>
2024-06-24 14:53:42 -0500

    updated three compartment

--- amount_drug_three_compartment.py
+++ amount_drug_three_compartment.py
@@ -1,15 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-<<<<<<< HEAD
 import scipy
 from scipy.integrate import solve_ivp
-=======
->>>>>>> ac8290694fc74329391800425ac0d5ed74b6d2dd
 
 h = 0.01 #step size
 t = np.arange(0, 5 + h, h) #time values
 
-<<<<<<< HEAD
 #amount of drug in gi and three compartments
 def ab(t, gcpd, ka, k10, k12, k21, k13, k31):
     gi, c, p, dt = gcpd
@@ -17,23 +13,6 @@ def ab(t, gcpd, ka, k10, k12, k21, k13, k31):
             ka * gi + k21 * p + k31 * dt - k10 * c - k12 * c - k13 * dt,
             k12 * c - k21 * p,
             k13 * c - k31 * dt]  
-=======
-#amount of drug in gastrointestinal tract
-def agi(t, gi):
-    return - 1 * ka * gi
-
-#amount of drug in central compartment
-def ac (t, gi, c, p, dt):
-    return ka * gi + k21 * p + k31 * dt - k10 * c - k12 * c - k13 * dt
-
-#amount of drug in peripheral compartment
-def ap (t, p, c):
-    return k12 * c - k21 * p
-
-#amount of drug in deep tissue compartment
-def adt (t, dt, c):
-    return k13 * c - k31 * dt
->>>>>>> ac8290694fc74329391800425ac0d5ed74b6d2dd
 
 d = 100 #mg
 gi0 = d #initial condition for gi
@@ -44,7 +23,6 @@ ka = 4 #absorption constant
 k10 = 3 #elimination constant
 k12 = 2 #central to peripheral constant
 k21 = 1 #peripheral to central constant
-<<<<<<< HEAD
 k13 = 0.8 #central to deep tissue constant
 k31 = 0.4 #deep tissue to central constant
 
@@ -91,32 +69,4 @@ plt.ylabel('Amount of Drug (mg)')
 plt.legend()
 plt.grid()
                 
-=======
-k13 = 2 #central to deep tissue constant
-k31 = 1 #deep tissue to central constant
-
-gi = np.zeros(len(t))
-gi[0] = gi0
-c = np.zeros(len(t))
-c[0] = c0
-p = np.zeros(len(t))
-p[0] = p0
-dt = np.zeros(len(t))
-dt[0] = dt0
-
-for i in range(0, len(t) - 1):
-    gi[i + 1] = gi[i] + h * agi(t[i], gi[i])
-    c[i + 1] = c[i] + h * ac(t[i], gi[i], c[i], p[i], dt[i])
-    p[i + 1] = p[i] + h * ap(t[i], p[i], c[i])
-    dt[i + 1] = dt[i] + h * adt(t[i], dt[i], c[i])
-
-plt.figure(figsize = (12, 8))
-plt.plot(t, c, 'r--', label='Approximate')
-plt.plot(t, (d * ka * (np.exp(-1 * k10 * t) - np.exp(-1 * ka * t))) / (ka - k10), 'b', label='Exact')
-plt.title('Amount of Drug in Central Compartment Over Time')
-plt.xlabel('Time')
-plt.ylabel('Amount of Drug in Body')
-plt.legend()
-plt.grid()
->>>>>>> ac8290694fc74329391800425ac0d5ed74b6d2dd
 plt.show()
