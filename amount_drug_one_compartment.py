bd30f6028738161ff07cdfd9db3eefacf1d69301
Smriti Kumar <mailsmritikumar@gmail.com>
2024-06-24 14:43:34 -0500

    added scipy integrate and combined equations

--- amount_drug_one_compartment.py
+++ amount_drug_one_compartment.py
@@ -1,16 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy
+from scipy.integrate import solve_ivp
 
 h = 0.01 #step size
 t = np.arange(0, 5 + h, h) #time values
 
-#amount of drug in gastrointestinal tract
-def agi(t, gi):
-    return - 1 * ka * gi
-
-#amount of drug in body
-def ab (t, gi, b):
-    return ka * gi - k * b
+#amount of drug in gi and body
+def adrug(t, gb, ka, k):
+    gi, b = gb
+    return [- 1 * ka * gi,
+            ka * gi - k * b]
 
 d = 100 #mg
 ka = 3 #absorption constant
@@ -18,21 +18,40 @@ gi0 = d #initial condition
 b0 = 0 #initial condition
 k = 2 #elimination constant
 
-gi = np.zeros(len(t))
-gi[0] = gi0
-b = np.zeros(len(t))
-b[0] = b0
+gb = np.zeros((len(t), 2))
+gb[0,:] = [gi0, b0]
 
 for i in range(0, len(t) - 1):
-    gi[i + 1] = gi[i] + h * agi(t[i], gi[i])
-    b[i + 1] = b[i] + h * ab(t[i], gi[i], b[i])
+    gi, b = gb[i]
+    agi, ab = adrug(t[i], gb[i], ka, k)
+    gb[i + 1] = [gi + h * agi, b+ h * ab]
+
+gi = gb[:, 0]
+b = gb[:, 1]
+
+scipy_solved = solve_ivp(adrug, (0, 5), gb[0,:], args=(ka, k), t_eval=t)
+
+solved_gi = scipy_solved.y[0]
+solved_b = scipy_solved.y[1]
 
 plt.figure(figsize = (12, 8))
-plt.plot(t, b, 'r--', label='Approximate')
-plt.plot(t, (d * ka * (np.exp(-1 * k * t) - np.exp(-1 * ka * t))) / (ka - k), 'b', label='Exact')
-plt.title('Amount of Drug in Body Over Time')
+
+plt.subplot(121)
+plt.plot(t, gi, 'b--', label="GI - Euler's")
+plt.plot(t, b, 'r--', label="Body - Euler's")
+plt.title("Amount of Drug Over Time - Euler's")
 plt.xlabel('Time')
-plt.ylabel('Amount of Drug in Body')
+plt.ylabel('Amount of Drug (mg)')
 plt.legend()
 plt.grid()
+
+plt.subplot(122)
+plt.plot(t, solved_gi, 'b', label='GI - SciPy Integrate')
+plt.plot(t, solved_b, 'r', label='Body - SciPy Integrate')
+plt.title('Amount of Drug Over Time - SciPy Integrate')
+plt.xlabel('Time')
+plt.ylabel('Amount of Drug (mg)')
+plt.legend()
+plt.grid()
+
 plt.show()
--- amount_drug_three_compartment.py
+++ amount_drug_three_compartment.py
@@ -6,60 +6,67 @@ from scipy.integrate import solve_ivp
 h = 0.01 #step size
 t = np.arange(0, 5 + h, h) #time values
 
-#amount of drug in gi and two compartments
-def ab (t, gcp, ka, k10, k12, k21):
-    gi, c, p = gcp
+#amount of drug in gi and three compartments
+def ab(t, gcpd, ka, k10, k12, k21, k13, k31):
+    gi, c, p, dt = gcpd
     return [- 1 * ka * gi,
-            ka * gi + k21 * p - k10 * c - k12 * c,
-            k12 * c - k21 * p]
+            ka * gi + k21 * p + k31 * dt - k10 * c - k12 * c - k13 * dt,
+            k12 * c - k21 * p,
+            k13 * c - k31 * dt]  
 
 d = 100 #mg
 gi0 = d #initial condition for gi
 c0 = 0 #initial condition for central
 p0 = 0 #initial condition for peripheral
+dt0 = 0 #initial condition for deep tissue
 ka = 4 #absorption constant
 k10 = 3 #elimination constant
 k12 = 2 #central to peripheral constant
 k21 = 1 #peripheral to central constant
+k13 = 0.8 #central to deep tissue constant
+k31 = 0.4 #deep tissue to central constant
 
-gcp = np.zeros((len(t), 3))
-gcp[0,:] = [gi0, c0, p0]
+gcpd = np.zeros((len(t), 4))
+gcpd[0,:] = [gi0, c0, p0, dt0]
 
 for i in range(0, len(t) - 1):
-    gi, c, p = gcp[i]
-    agi, ac, ap = ab(t[i], gcp[i], ka, k10, k12, k21)
-    gcp[i + 1] = [gi + h * agi, c + h * ac, p + h * ap]
+    gi, c, p, dt = gcpd[i]
+    agi, ac, ap, adt = ab(t[i], gcpd[i], ka, k10, k12, k21, k13, k31)
+    gcpd[i + 1] = [gi + h * agi, c + h * ac, p + h * ap, dt + h * adt]
 
-gi = gcp[:, 0]
-c = gcp[:, 1]
-p = gcp[:, 2]
+gi = gcpd[:, 0]
+c = gcpd[:, 1]
+p = gcpd[:, 2]
+dt = gcpd[:, 3]
 
-scipy_solved = solve_ivp(ab, (0, 5), gcp[0,:], args=(ka, k10, k12, k21), t_eval=t)
+scipy_solved = solve_ivp(ab, (0, 5), gcpd[0,:], args=(ka, k10, k12, k21, k13, k31), t_eval=t)
 
 solved_gi = scipy_solved.y[0]
 solved_c = scipy_solved.y[1]
 solved_p = scipy_solved.y[2]
-
-plt.figure(figsize = (12, 8))
+solved_dt = scipy_solved.y[3]
 
 plt.subplot(121)
 plt.plot(t, gi, 'b--', label="GI - Euler's")
 plt.plot(t, c, 'r--', label="Central - Euler's")
 plt.plot(t, p, 'g--', label="Peripheral - Euler's")
+plt.plot(t, dt, 'm--', label="Deep Tissue - Euler's")
 plt.title("Amount of Drug Over Time - Euler's")
 plt.xlabel('Time')
 plt.ylabel('Amount of Drug (mg)')
 plt.legend()
 plt.grid()
 
+
 plt.subplot(122)
 plt.plot(t, solved_gi, 'b', label='GI - SciPy Integrate')
 plt.plot(t, solved_c, 'r', label='Central - SciPy Integrate')
 plt.plot(t, solved_p, 'g', label='Peripheral - SciPy Integrate')
+plt.plot(t, solved_dt, 'm', label='Deep Tissue - SciPy Integrate')
 plt.title('Amount of Drug Over Time - SciPy Integrate')
 plt.xlabel('Time')
 plt.ylabel('Amount of Drug (mg)')
 plt.legend()
 plt.grid()
-
+                
 plt.show()
