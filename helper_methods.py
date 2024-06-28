d87e4e406b80f3f2f6d2e9e552fe45ad0b42b731
Smriti Kumar <mailsmritikumar@gmail.com>
2024-06-27 21:14:26 -0500

    added docstring

--- helper_methods.py
+++ helper_methods.py
@@ -1,8 +1,11 @@
 def euler_func(initial_conditions, eq_function, time_step):
     """
-    states:
-        inputs  .....
-        outputs ....
-        args = (.....)
+    performs one step of euler's method.
+    inputs:
+        initial_conditions of function
+        eq_function representing the diff eq
+        time_step for euler step
+    outputs:
+        update after one time step
     """
     return initial_conditions + time_step * eq_function
